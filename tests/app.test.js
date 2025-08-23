a2c2cc7b4b31b251210279e054632eb1483b7e13
willian-cmd <willianmartinezhernandez@gmail.com>
2025-08-23 16:53:58 -0400

    Primer commit - Hola Mundo app

---
+++ tests/app.test.js
@@ -0,0 +1,11 @@
+const request = require("supertest");
+const app = require("../src/app");
+
+describe("GET /", () => {
+  it("debe responder con Hola Mundo desde DevOps ��", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toBe(200);
+    expect(res.text).toBe("Hola Mundo desde DevOps 🚀");
+  });
+});
+
