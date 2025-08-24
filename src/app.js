fb603a3aa82d37b023b2831634239b459ebe8df3
willian-cmd <willianmartinezhernandez@gmail.com>
2025-08-24 12:28:45 -0400

    Refactor: separar app y server para tests rápidos

--- src/app.js
+++ src/app.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const app = express();
 
-// Ruta principal
 app.get("/", (req, res) => {
   res.send("Hola Mundo desde DevOps 🚀");
 });
 
-// Configuración del puerto
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
-
-// Exportamos app para poder hacer pruebas
 module.exports = app;
